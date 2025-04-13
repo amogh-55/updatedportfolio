2574f908e07f59ff15f57cc80cf5ac62def78075
amogh <amoghamith22@gmail.com>
2025-04-13 17:34:33 +0530

    changed text size in all screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,27 +7,28 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[80px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[80px] max-w-6xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div className="z-10">
+        <div className="z-10 max-w-3xl">
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Amogh</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          Software Developer Creating Seamless Interfaces Where Code  <br className='sm:block hidden' />
-         Meets Creativity and Brings Ideas to reality 
+            Software Developer Creating Seamless Interfaces <br className='sm:block hidden' />
+            Where Code Meets Creativity and <br className='lg:block hidden' />
+            Brings Ideas to Reality
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
 
-      <div style={{ position: 'absolute', bottom: '-8px', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 20 }}>
+      <div style={{ position: 'absolute', bottom: '-20px', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 20 }}>
         <a href='#about' className="block p-4">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
